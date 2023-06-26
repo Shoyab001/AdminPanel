0b32703340ae0a6f76dfe3b9320f21c3cc8b4a5d
Shoyab khan <mkshoyab82@gmail.com>
2023-06-26 12:35:20 +0530

    up to date

--- src/_services/auth.service.js
+++ src/_services/auth.service.js
@@ -1,16 +1,67 @@
-import apiPath from '../api/api_path'
+import apipath from '../api/api_path'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
-export async function LoginPage(body) {
+export async function Loginapi(body) {
   try {
-    const res = await axios.post(apiPath.BASE_URL + apiPath.LOGIN, body)
-    // console.log(res)
-    return {
-      data: res.data,
-    }
-    // console.log('data')
+    const res = await axios.post(apipath.BASE_URL + apipath.LOGIN, body, {
+      headers: {
+        'x-testing-platform': 'web',
+        'x-testing-version': '1.0.0',
+        'Accept-Language': 'en',
+      },
+    })
+
+    return res?.data
   } catch (error) {
-    toast.error('Network error.')
+    return error?.res?.data
+  }
+}
+
+export async function Signup(body) {
+  try {
+    const res = await axios.post(apipath.BASE_URL + apipath.SIGNUP, body, {
+      headers: {
+        'x-testing-platform': 'web',
+        'x-testing-version': '1.0.0',
+        'accept-language': 'en',
+      },
+    })
+
+    return res?.data
+  } catch (error) {
+    return error?.res?.data
+  }
+}
+
+export async function Signuprestro(body) {
+  try {
+    const res = await axios.post(apipath.BASE_URL + apipath.SIGNUPRESTRO, body, {
+      headers: {
+        'x-testing-platform': 'web',
+        'x-testing-version': '1.0.0',
+        'accept-language': 'en',
+      },
+    })
+
+    return res?.data
+  } catch (error) {
+    return error?.res?.data
+  }
+}
+
+export async function ChangePassword(body) {
+  try {
+    const res = await axios.post(apipath.BASE_URL + apipath.Changepassword, body, {
+      headers: {
+        'x-testing-platform': 'web',
+        'x-testing-version': '1.0.0',
+        'accept-language': 'en',
+      },
+    })
+
+    return res?.data
+  } catch (error) {
+    return error?.res?.data
   }
 }
